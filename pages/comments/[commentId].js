34169ceb012e88c3614a43f2ef8fc2a7fa05255a
Manthan Kuber <manthankuber873@gmail.com>
2022-04-19 18:51:43 +0530

    Pre-rendering and APIs

---
+++ pages/comments/[commentId].js
@@ -0,0 +1,41 @@
+import { comments } from "../../data/comments";
+
+function Comment({ comment }) {
+  return (
+    <div>
+      {comment.id} {comment.text}
+    </div>
+  );
+}
+
+export default Comment;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { commentId: "1" } },
+      { params: { commentId: "2" } },
+      { params: { commentId: "3" } },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { commentId } = params;
+
+  //Don't do const response = await fetch ... here
+  // Do not call your own API in here
+
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(commentId)
+  );
+  console.log(comment);
+
+  return {
+    props: {
+      comment,
+    },
+  };
+}
